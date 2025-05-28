d8f80685c69619451f088d9529389fe21b651dc0
Damião V. Souza <139881408+damiaovieiraa@users.noreply.github.com>
2025-05-28 13:20:40 -0300

    Update script.js url

--- public/script.js
+++ public/script.js
@@ -1,11 +1,12 @@
+const url = "https://sistema-musica-dj-3n18.onrender.com"
+
 document.getElementById("registerForm").addEventListener("submit", async (e) => {
     e.preventDefault()
 
     const email = document.getElementById("registerEmail").value
     const password = document.getElementById("registerPassword").value
 
-    const url = "http://localhost:3000/auth/register"
-    const response = await fetch(url, {
+    const response = await fetch(url + "/auth/register", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({email, password})
@@ -23,9 +24,8 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
 
     const email = document.getElementById("loginEmail").value
     const password = document.getElementById("loginPassword").value
-
-    const url = "http://localhost:3000/auth/login"
-    const response = await fetch(url, {
+    
+    const response = await fetch(url + "/auth/login", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({email, password})
